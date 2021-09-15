6c408ebf73ce2d8cc0cf7a366117617d71ca5c56
Maksym Havrylov <m.havrylov001@gmail.com>
2021-09-15 21:52:04 +0300

    modal

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  var currentFloor = 2;
+  var floorPath = $('.home-image path');
+  var counterUp = $('.counter-up');
+  var counterDown = $('.counter-down');
+  var modal = $('.modal');
+  var viewFlatsButton = $('.view-flats');
+  var modalCloseButton = $('.modal-close-button');
+
+  floorPath.on('mouseover', function () {
+    floorPath.removeClass('current-floor');
+    currentFloor = $(this).attr('data-floor');
+    $('.counter').text(currentFloor);
+  });
+
+  viewFlatsButton.on('click', toggleModal);
+  floorPath.on('click', toggleModal);
+  modalCloseButton.on('click', toggleModal);
+
+  counterUp.on('click', function () {
+    if (currentFloor < 18) {
+      currentFloor++;
+      var usCurrentFloor = currentFloor.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+      $('.counter').text(usCurrentFloor);
+      floorPath.removeClass('current-floor');
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
+    }
+  });
+
+  counterDown.on('click', function () {
+    if (currentFloor > 2) {
+      currentFloor--;
+      var usCurrentFloor = currentFloor.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
+      $('.counter').text(usCurrentFloor);
+      floorPath.removeClass('current-floor');
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
+    }
+  });
+
+  function toggleModal() {
+    modal.toggleClass('is-open');
+  }
+});
